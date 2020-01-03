8876ea596a5ab97d5679bc57bf2f609b0126b205
vennelatadi <59447318+vennelatadi@users.noreply.github.com>
2020-01-03 10:15:04 +0530

    Add files via upload

---
+++ function-arguments.js
@@ -0,0 +1,3 @@
+function math(add,subtract,multiply){
+return add+(subtract*multiply);}
+console.log(math(53,61,67));
---
+++ object-keys.js
@@ -0,0 +1,3 @@
+var car={make:'Honda', model:'Accord', year:2020}
+var keys=Object.keys(car)
+console.log(keys)
